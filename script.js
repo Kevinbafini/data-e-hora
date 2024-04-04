2cf509f4ea0c26bb11e514fa50eb6773841c5609
Kevinbafini <kevinbafinidaluz12@gmail.com>
2024-04-04 09:36:50 -0300

    add date js

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 
+const agora = new Date();
+const diaDaSemanaNumero = agora.getDay();
+const diasDaSemana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+const diaDaSemanaEscrito = diasDaSemana[diaDaSemanaNumero];
+const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+const mesEscrito = meses[agora.getMonth()];
+const anoNumerico = agora.getFullYear();
+
 const relogiodigital = setInterval(function time() {
     let dateToday = new Date();
     let hr = dateToday.getHours();
@@ -17,5 +25,10 @@ const relogiodigital = setInterval(function time() {
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = s;
+
+    data.innerHTML = `${diaDaSemanaEscrito}, ${diaDaSemanaNumero} de ${mesEscrito} de ${anoNumerico}`
+
+
+
 
 })
